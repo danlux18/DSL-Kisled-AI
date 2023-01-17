6ddc8a97aa0d5df4499f060aecfbf68b01d47ffe
GabrielCogne <gabriel.cogne@gmail.com>
2023-01-17 16:25:29 +0100

    Add Jupyter base generator

--- src/main/groovy/fr/kisled/Main.groovy
+++ src/main/groovy/fr/kisled/Main.groovy
@@ -2,16 +2,23 @@ package fr.kisled
 
 import fr.kisled.dsl.Parser
 import fr.kisled.dsl.generator.Generator
+import fr.kisled.dsl.generator.jupyter.JupyterGenerator
 import fr.kisled.dsl.generator.python.PyGenerator
 import fr.kisled.kernel.App
 
 class Main {
     static void main(String... args) {
         Parser parser = new Parser()
-        Generator generator = new PyGenerator()
+        Generator generator = new JupyterGenerator()
         for (String arg : args) {
             App app = parser.parse(new File(arg))
-            generator.generate(app)
+
+            File result = new File("../${app.name}.ipynb")
+            PrintStream output = new PrintStream(result)
+
+            generator.generate(app, output)
+
+            output.close()
         }
     }
 }
